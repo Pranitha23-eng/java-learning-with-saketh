01a0d359a44bd1b3a35bb79ca2ad860f016d3516
Pranitha23-eng <62777591+Pranitha23-eng@users.noreply.github.com>
2020-05-11 16:42:44 -0400

    pool

--- firstjavaproject/src/HeadFirstChapter1/Jumble3.java
+++ firstjavaproject/src/HeadFirstChapter1/Jumble3.java
@@ -2,8 +2,8 @@ package HeadFirstChapter1;
 
 public class Jumble3 {
 	public static void main(String[] args) {
-		 int x = 5;
-		 while ( x > 1 ) {
+		 int x = 3;
+		 if( x > 1 ) {
 		 x = x - 1;
 		 if ( x < 3) {
 		 System.out.println( 'x');
