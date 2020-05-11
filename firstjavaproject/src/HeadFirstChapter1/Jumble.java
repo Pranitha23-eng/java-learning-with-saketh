963b6da43fa97069f25df434facda3d6c5322350
Pranitha23-eng <62777591+Pranitha23-eng@users.noreply.github.com>
2020-05-11 15:47:07 -0400

    jumblee

---
+++ firstjavaproject/src/HeadFirstChapter1/Jumble.java
@@ -0,0 +1,15 @@
+package HeadFirstChapter1;
+
+public class Jumble {
+	 
+		 public static void main(String [] args) {
+		 int x = 2;
+		 while ( x < 10 ) {
+		 if ( x > 3) {
+		 System.out.println('x');
+
+
+}
+}
+		 }
+}
---
+++ firstjavaproject/src/HeadFirstChapter1/Jumble2.java
@@ -0,0 +1,15 @@
+package HeadFirstChapter1;
+
+public class Jumble2 {
+	public static void main(String [] args) {
+		 int x = 3;
+		 while ( x > 1 ) {
+		 x = x - 1;
+		 if ( x < 3) {
+		 System.out.println('x');
+
+}
+		 }
+	}
+}
+
