1f0cdd920a645c968935eaa29a6f6a79e8a3e535
Pranitha23-eng <62777591+Pranitha23-eng@users.noreply.github.com>
2020-05-11 15:29:22 -0400

    shuffle

---
+++ firstjavaproject/src/HeadFirstChapter1/Shuffle.java
@@ -0,0 +1,30 @@
+package HeadFirstChapter1;
+
+	class Shuffle1 {
+		
+		 public static void main(String [] args) {
+			 int x = 3;
+			 while (x > 0)
+			 {
+			 if (x > 2) {
+				 System.out.print('a');
+				 }
+			 x = x - 1;
+			 if (x == 2) {
+			 System.out.print('-');
+			 
+				 System.out.print('b');
+				 System.out.print('c');
+				 }
+			 x = x - 1;
+			 if (x == 1) {
+				 System.out.print('-');
+				 System.out.print('d');
+				 
+				 }
+
+}
+		 }
+	}
+	
+	
