74d98f6b94c977c154d6ceaea338352578734a4e
Pranitha23-eng <62777591+Pranitha23-eng@users.noreply.github.com>
2020-05-16 13:39:21 -0400

    time

---
+++ Secondjavaproject/src/com/HFJ4/MethodsUseIV/Clock.java
@@ -0,0 +1,12 @@
+package com.HFJ4.MethodsUseIV;
+
+public class Clock {
+		  int time;
+		public void setTime(int t) {
+		 time = t;
+		 }
+		 public int getTime() {
+		 return time;
+		 }
+		}
+		
---
+++ Secondjavaproject/src/com/HFJ4/MethodsUseIV/ClockTestDrive.java
@@ -0,0 +1,14 @@
+package com.HFJ4.MethodsUseIV;
+
+public class ClockTestDrive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Clock c = new Clock();
+		 c.setTime(1245);
+		 int tod = c.getTime();
+		 System.out.println("time: " + tod);
+
+	}
+
+}
